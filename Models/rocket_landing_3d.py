4c24688e6aa37086d18caefd9404b1a3f10e9e08
Aaron John Sabu <aaronjohnsabu1999@gmail.com>
2024-07-02 11:19:47 -0700

    lil import issue

--- Models/rocket_landing_3d.py
+++ Models/rocket_landing_3d.py
@@ -1,9 +1,13 @@
 import sympy as sp
 import numpy as np
 import cvxpy as cvx
+
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
+
 from utils import euler_to_quat
 from global_parameters import K
-
 
 def skew(v):
     return sp.Matrix([
